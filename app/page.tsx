c55e64676c230eacb9a31d1f8ea23b654fd89d3c
idk321687-coder <idk321687@gmail.com>
2026-08-27 12:32:01 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <div className="min-h-screen text-white text-xl flex flex-col items-center justify-center bg-gradient-to-br from-yellow-300 via-blue-400 to-blue-500 space-y-5">
-      <h1 className="text-6xl font-bold">Portfolio</h1>
-      <p>This page is supposed to explain the website.</p>
+      <h1 className="text-6xl font-bold">Homepage</h1>
+      <p>Welcome to the homepage. COMING SOON!</p>
     </div>
   )
 }
